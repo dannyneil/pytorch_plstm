96cc3b6f02d298cac433e1c479fb03ba41b824df
Dan Neil <daniel.neil@benevolent.ai>
2021-03-25 12:08:45 -0400

    Longer train.

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ def main():
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+    parser.add_argument('--epochs', type=int, default=15, metavar='N',
                         help='number of epochs to train (default: 14)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
